f63c807133bcd4f7c1320085f22c408b571a58fa
Paul van Nugteren <PMvanNugteren@gmail.com>
2012-12-19 18:32:45 +0100

    Added a method for adding the textView as a subview to the cell when it is selected, implemented heightForRowAtIndexPath based on the amount of text and font, implemented textViewDidChange to grow the cell dynamically, this especially is to be improved

--- JTGestureBasedTableViewDemo/ViewController.m
+++ JTGestureBasedTableViewDemo/ViewController.m
@@ -18,20 +18,41 @@
 @property (nonatomic, strong) JTTableViewGestureRecognizer *tableViewRecognizer;
 @property (nonatomic, strong) id grabbedObject;
 
+@property (nonatomic, strong) UITextView *taskEntry; //We need just one textView just for entering text into one cell at a time. Needs to be strong because when weak taskEntry is released (ARC) when tableview:didSelectRowForIndexPath: ended
+@property (nonatomic, strong) NSIndexPath *currentIndexPath;//Needs to be strong
+@property (nonatomic) NSUInteger textViewPreviousNumberOfCharacters; //We need to know if the text increased or decreased in textViewDidChange
+
+
 - (void)moveRowToBottomForIndexPath:(NSIndexPath *)indexPath;
 
 @end
 
 @implementation ViewController
+
 @synthesize rows;
 @synthesize tableViewRecognizer;
 @synthesize grabbedObject;
+@synthesize taskEntry = _taskEntry;
+@synthesize currentIndexPath = _currentIndexPath;
+
 
 #define ADDING_CELL @"Continue..."
 #define DONE_CELL @"Done"
 #define DUMMY_CELL @"Dummy"
-#define COMMITING_CREATE_CELL_HEIGHT 60
-#define NORMAL_CELL_FINISHING_HEIGHT 60
+#define COMMITING_CREATE_CELL_HEIGHT 60.0f
+#define NORMAL_CELL_FINISHING_HEIGHT 60.0f
+
+#define FONT_SIZE 14.0f
+#define CELL_CONTENT_WIDTH 320.0f
+#define CELL_CONTENT_MARGIN 12.0f
+#define CELL_MAX_CONTENT_HEIGHT 20000.0f
+
+#define TEXT_VIEW_OFF_SET_X 2 //These are the offsets to make the text
+#define TEXT_VIEW_OFF_SET_Y 10 //in the textview appear in the same place as the cell's text
+
+
+#define CHARACTERS_PER_LINE  40 //Rewrite by using CELL_CONTENT_WIDTH etc.
+#define CELL_HEIGHT_CHUNK_PER_LINE 20 //Rewrite by using CELL_CONTENT_WIDTH etc.
 
 #pragma mark - View lifecycle
 
@@ -51,19 +72,21 @@
                  @"Drag down to create a new cell",
                  @"Pinch two rows apart to create cell",
                  @"Long hold to start reorder cell",
+                 @"More then 27 characters on this line, word word word word word word word word word word word word word word word word word word", //test item!
                  nil];
-
-
+    
+    
     // Setup your tableView.delegate and tableView.datasource,
     // then enable gesture recognition in one line.
     self.tableViewRecognizer = [self.tableView enableGestureTableViewWithDelegate:self];
     
     self.tableView.backgroundColor = [UIColor blackColor];
-    self.tableView.separatorStyle  = UITableViewCellSeparatorStyleNone;
-    self.tableView.rowHeight       = NORMAL_CELL_FINISHING_HEIGHT;
+    self.tableView.separatorStyle  = UITableViewCellSeparatorStyleSingleLine;
+    self.tableView.separatorColor  = [UIColor colorWithRed:0.75 green:0.26 blue:0.14 alpha:1];
+    //self.tableView.rowHeight       = NORMAL_CELL_FINISHING_HEIGHT; //No we want dynamic rowheights for multple lines of text!
 }
 
-#pragma mark Private Method
+#pragma mark - Private Method
 
 - (void)moveRowToBottomForIndexPath:(NSIndexPath *)indexPath {
     [self.tableView beginUpdates];
@@ -71,16 +94,16 @@
     id object = [self.rows objectAtIndex:indexPath.row];
     [self.rows removeObjectAtIndex:indexPath.row];
     [self.rows addObject:object];
-
+    
     NSIndexPath *lastIndexPath = [NSIndexPath indexPathForRow:[self.rows count] - 1 inSection:0];
     [self.tableView moveRowAtIndexPath:indexPath toIndexPath:lastIndexPath];
-
+    
     [self.tableView endUpdates];
-
+    
     [self.tableView performSelector:@selector(reloadVisibleRowsExceptIndexPath:) withObject:lastIndexPath afterDelay:JTTableViewRowAnimationDuration];
 }
 
-#pragma mark UITableViewDatasource
+#pragma mark - UITableViewDatasource
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     return [self.rows count];
@@ -91,13 +114,12 @@
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-
     NSObject *object = [self.rows objectAtIndex:indexPath.row];
-    UIColor *backgroundColor = [[UIColor redColor] colorWithHueOffset:0.12 * indexPath.row / [self tableView:tableView numberOfRowsInSection:indexPath.section]];
+    UIColor *backgroundColor = [[UIColor colorWithRed:1 green:0.25 blue:0.15 alpha:1] colorWithHueOffset: (0.12 * indexPath.row / [self tableView:tableView numberOfRowsInSection:indexPath.section] )];
     if ([object isEqual:ADDING_CELL]) {
         NSString *cellIdentifier = nil;
         TransformableTableViewCell *cell = nil;
-
+        
         // IndexPath.row == 0 is the case we wanted to pick the pullDown style
         if (indexPath.row == 0) {
             cellIdentifier = @"PullDownTableViewCell";
@@ -106,7 +128,7 @@
             if (cell == nil) {
                 cell = [TransformableTableViewCell transformableTableViewCellWithStyle:TransformableTableViewCellStylePullDown
                                                                        reuseIdentifier:cellIdentifier];
-                cell.textLabel.adjustsFontSizeToFitWidth = YES;
+                cell.textLabel.lineBreakMode = UILineBreakModeWordWrap; // Example: The brown fox...dog
                 cell.textLabel.textColor = [UIColor whiteColor];
                 cell.textLabel.textAlignment = UITextAlignmentCenter;
             }
@@ -131,16 +153,17 @@
             cell.contentView.backgroundColor = [UIColor clearColor];
             cell.detailTextLabel.text = @" ";
             return cell;
-
+            
         } else {
-            // Otherwise is the case we wanted to pick the pullDown style
+            // Otherwise is the case we wanted to pick the unfolding style
             cellIdentifier = @"UnfoldingTableViewCell";
             cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
-
+            
             if (cell == nil) {
                 cell = [TransformableTableViewCell transformableTableViewCellWithStyle:TransformableTableViewCellStyleUnfolding
                                                                        reuseIdentifier:cellIdentifier];
-                cell.textLabel.adjustsFontSizeToFitWidth = YES;
+                cell.textLabel.adjustsFontSizeToFitWidth = NO;
+                cell.textLabel.lineBreakMode = UILineBreakModeWordWrap;
                 cell.textLabel.textColor = [UIColor whiteColor];
                 cell.textLabel.textAlignment = UITextAlignmentCenter;
             }
@@ -158,20 +181,22 @@
             cell.detailTextLabel.text = @" ";
             return cell;
         }
-    
+        
     } else {
-
+        
         static NSString *cellIdentifier = @"MyCell";
         UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
         if (cell == nil) {
             cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellIdentifier];
-            cell.textLabel.adjustsFontSizeToFitWidth = YES;
+            cell.textLabel.text = [NSString stringWithFormat:@"%@", (NSString *)object];;
+            cell.textLabel.numberOfLines = 4; //After that the textView should start to scroll
             cell.textLabel.backgroundColor = [UIColor clearColor];
             cell.selectionStyle = UITableViewCellSelectionStyleNone;
         }
-
         cell.textLabel.text = [NSString stringWithFormat:@"%@", (NSString *)object];
-        cell.detailTextLabel.text = @" ";
+        cell.textLabel.lineBreakMode = UILineBreakModeWordWrap;
+        cell.textLabel.backgroundColor = [UIColor clearColor];
+        cell.selectionStyle = UITableViewCellSelectionStyleNone;
         if ([object isEqual:DONE_CELL]) {
             cell.textLabel.textColor = [UIColor grayColor];
             cell.contentView.backgroundColor = [UIColor darkGrayColor];
@@ -184,21 +209,83 @@
         }
         return cell;
     }
-
+    
 }
 
-#pragma mark UITableViewDelegate
+#pragma mark - UITableViewDelegate
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
-    return NORMAL_CELL_FINISHING_HEIGHT;
+    //Add to NORMAL_CELL_FINISHING_HEIGHT chunk when more then 27 characters per line are added something TOTAL_AMOUNT_OF_TEXT/27 where TOTAL_AMOUNT_OF_TEXT is an int
+    NSString *text = [self.rows objectAtIndex:indexPath.row];
+    //CGSize constraint = CGSizeMake(CELL_CONTENT_WIDTH - (CELL_CONTENT_MARGIN * 2), CELL_MAX_CONTENT_HEIGHT);
+    
+    CGSize size = [text sizeWithFont:[UIFont systemFontOfSize:FONT_SIZE]constrainedToSize: CGSizeMake(320-5, 4000)lineBreakMode:UILineBreakModeWordWrap];
+    
+    CGFloat height = MAX(size.height, NORMAL_CELL_FINISHING_HEIGHT);
+    
+    return (height + (CELL_CONTENT_MARGIN * 2));
+    
+    // if numberOfCharacters -1 < CHARACTERS_PER_LINE (numberOfCharacters -1) / CHARACTERS_PER_LINE == 0 ie. everything fits on one line thus we return the NORMAL_CELL_FINISHING_HEIGHT
+    //return (numberOfCharacters -1) / CHARACTERS_PER_LINE * CELL_HEIGHT_CHUNK_PER_LINE + NORMAL_CELL_FINISHING_HEIGHT; //Bug: Returns int!
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-    NSLog(@"tableView:didSelectRowAtIndexPath: %@", indexPath);
+    //[tableView scrollToRowAtIndexPath:indexPath atScrollPosition:UITableViewScrollPositionTop animated:YES]; //first scroll to the top (but it doesn't work and no idea why!!?)
+    if (self.taskEntry) self.taskEntry = nil; // release it? We just want one TextView
+    self.taskEntry = [[UITextView alloc]initWithFrame:CGRectMake(TEXT_VIEW_OFF_SET_X, TEXT_VIEW_OFF_SET_Y, 320-TEXT_VIEW_OFF_SET_X, [self tableView:tableView heightForRowAtIndexPath:indexPath]-TEXT_VIEW_OFF_SET_Y)]; //The text offset to make it appear that the taskEntry.text position equals the cell.textLabel.text position
+    //
+    [[tableView cellForRowAtIndexPath:indexPath] addSubview:self.taskEntry]; //Add it only when we need it
+    self.taskEntry.delegate = self; //toDO: This suffices once->move to somewhere global
+    self.taskEntry.backgroundColor = [UIColor clearColor];
+    UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+    self.taskEntry.textColor = [UIColor whiteColor];
+    self.taskEntry.font = cell.textLabel.font;
+    self.taskEntry.text = [NSString stringWithFormat:@"%@", (NSString *)[self.rows objectAtIndex:indexPath.row]];
+    //NSLog(@"text: %@", self.taskEntry.text);
+    cell.textLabel.text = nil;
+    self.currentIndexPath = indexPath;
+    self.textViewPreviousNumberOfCharacters = self.taskEntry.text.length;
+    NSLog(@"self.currentIndexPath.row %d", self.currentIndexPath.row);
+    [self.taskEntry becomeFirstResponder];
 }
 
-#pragma mark -
-#pragma mark JTTableViewGestureAddingRowDelegate
+#pragma mark - TextViewDelegate
+
+- (void)textViewDidChange:(UITextView *)textView {
+    //If more text then fits on a single line (remainder zero) grow cell
+    CGSize size = [textView.text sizeWithFont:[UIFont systemFontOfSize:FONT_SIZE]constrainedToSize: CGSizeMake(320-5, 4000)lineBreakMode:UILineBreakModeWordWrap];
+    int height = size.height;
+    NSLog(@"height: %i", height);
+    if (height > 18 && height % 18 == 0  && self.textViewPreviousNumberOfCharacters < textView.text.length) { //ie. we're adding text
+        //Buffer the string before it gets reloaded by the tableview
+        [self.rows replaceObjectAtIndex: self.currentIndexPath.row withObject: textView.text];
+        self.textViewPreviousNumberOfCharacters = textView.text.length; //So that we know if the text increased instead of decreased
+        [self.taskEntry removeFromSuperview]; //see below
+        [self.tableView reloadRowsAtIndexPaths: [self.tableView indexPathsForVisibleRows ] withRowAnimation:UITableViewRowAnimationNone];
+        [[self.tableView cellForRowAtIndexPath:self.currentIndexPath] addSubview:self.taskEntry]; //Reloading a row causes the table view to ask its data source for a new cell for that row. So we must re-add it:
+        UITableViewCell *selectedCell = [self.tableView cellForRowAtIndexPath:self.currentIndexPath];
+        selectedCell.textLabel.text = nil;
+        self.taskEntry.frame = CGRectMake(0, 0, selectedCell.contentView.frame.size.width, selectedCell.contentView.frame.size.height);//Increase the size of the frame of the textView
+        [self.taskEntry becomeFirstResponder];
+    }
+    if (textView.text.length >= 18 && textView.text.length % 18 == 0 && self.textViewPreviousNumberOfCharacters > textView.text.length) { //ie. we're removing text
+        //Buffer the string before it gets reloaded by the tableview
+        [self.rows replaceObjectAtIndex: self.currentIndexPath.row withObject: textView.text];
+        self.textViewPreviousNumberOfCharacters = textView.text.length; //So that we know if the text decreased instead of increased
+        [self.taskEntry removeFromSuperview];
+        //reload the visible cells
+        [self.tableView reloadRowsAtIndexPaths: [self.tableView indexPathsForVisibleRows ] withRowAnimation:UITableViewRowAnimationNone];
+        //re-add the textView as a subview after the reload since this is a new cell
+        [[self.tableView cellForRowAtIndexPath:self.currentIndexPath] addSubview:self.taskEntry];
+        UITableViewCell *selectedCell = [self.tableView cellForRowAtIndexPath:self.currentIndexPath];
+        selectedCell.textLabel.text = nil;
+        self.taskEntry.frame = CGRectMake(0, 0, selectedCell.contentView.frame.size.width, selectedCell.contentView.frame.size.height);
+        [self.taskEntry becomeFirstResponder];
+    }
+    
+}
+
+#pragma mark - JTTableViewGestureAddingRowDelegate
 
 - (void)gestureRecognizer:(JTTableViewGestureRecognizer *)gestureRecognizer needsAddRowAtIndexPath:(NSIndexPath *)indexPath {
     [self.rows insertObject:ADDING_CELL atIndex:indexPath.row];
@@ -207,7 +294,7 @@
 - (void)gestureRecognizer:(JTTableViewGestureRecognizer *)gestureRecognizer needsCommitRowAtIndexPath:(NSIndexPath *)indexPath {
     [self.rows replaceObjectAtIndex:indexPath.row withObject:@"Added!"];
     TransformableTableViewCell *cell = (id)[gestureRecognizer.tableView cellForRowAtIndexPath:indexPath];
-
+    
     BOOL isFirstCell = indexPath.section == 0 && indexPath.row == 0;
     if (isFirstCell && cell.frame.size.height > COMMITING_CREATE_CELL_HEIGHT * 2) {
         [self.rows removeObjectAtIndex:indexPath.row];
@@ -237,17 +324,17 @@
 
 - (void)gestureRecognizer:(JTTableViewGestureRecognizer *)gestureRecognizer didEnterEditingState:(JTTableViewCellEditingState)state forRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [self.tableView cellForRowAtIndexPath:indexPath];
-
+    
     UIColor *backgroundColor = nil;
     switch (state) {
         case JTTableViewCellEditingStateMiddle:
-            backgroundColor = [[UIColor redColor] colorWithHueOffset:0.12 * indexPath.row / [self tableView:self.tableView numberOfRowsInSection:indexPath.section]];
+            backgroundColor = [UIColor tableView: self.tableView CellBackgroundColor: [UIColor tableViewCellHighestPriorityDefaultBackgroundColor] AtIndexPath: indexPath];
             break;
         case JTTableViewCellEditingStateRight:
-            backgroundColor = [UIColor greenColor];
+            backgroundColor = [UIColor EditingStateBackgroundColorForDoneCell];
             break;
         default:
-            backgroundColor = [UIColor darkGrayColor];
+            backgroundColor = [UIColor DoneCellBackgroundColor];
             break;
     }
     cell.contentView.backgroundColor = backgroundColor;
@@ -266,11 +353,11 @@
     
     
     NSIndexPath *rowToBeMovedToBottom = nil;
-
+    
     [tableView beginUpdates];
     if (state == JTTableViewCellEditingStateLeft) {
         // An example to discard the cell at JTTableViewCellEditingStateLeft
-        [self.rows removeObjectAtIndex:indexPath.row];
+        [self.rows removeObjectAtIndex:indexPath.row]; //toDo future: add the object to "Today" list
         [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationLeft];
     } else if (state == JTTableViewCellEditingStateRight) {
         // An example to retain the cell at commiting at JTTableViewCellEditingStateRight
@@ -282,11 +369,11 @@
         // - [JTTableViewGestureDelegate gestureRecognizer:commitEditingState:forRowAtIndexPath:]
     }
     [tableView endUpdates];
-
-
+    
+    
     // Row color needs update after datasource changes, reload it.
     [tableView performSelector:@selector(reloadVisibleRowsExceptIndexPath:) withObject:indexPath afterDelay:JTTableViewRowAnimationDuration];
-
+    
     if (rowToBeMovedToBottom) {
         [self performSelector:@selector(moveRowToBottomForIndexPath:) withObject:rowToBeMovedToBottom afterDelay:JTTableViewRowAnimationDuration * 2];
     }
